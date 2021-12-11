cbaf0d5e65590ae282b2d157b7bdd74c62f6b11e
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-11 13:56:43 +0900

    cache file

--- main.jl
+++ main.jl
@@ -2,8 +2,18 @@ using HTTP
 using Gumbo
 
 url = "https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads"
+cache_file = "cache.html"
 
-r = HTTP.request("GET", url)
-if r.status != 200
-    return
+if isfile(cache_file)
+    body = open(f->read(f, String), cache_file)
+else
+    r = HTTP.request("GET", url)
+    if r.status != 200
+        return
+    end
+
+    body = String(r.body)
+    open("cache.html", "w") do io
+        write(io, body)
+    end
 end
